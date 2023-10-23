e526bd4351f14cabbf7631f15d33a3b9d19f8bb8
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-10-23 00:14:12 -0400

    Iterating over a pandas DataFrame

--- main.py
+++ main.py
@@ -24,6 +24,19 @@ import random
 # new_dict =  {new_key: new_value for (key, value) in diction.items()}
 # new_dict =  {new_key: new_value for (key, value) in diction.items() if test }
 
-names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
-students_score = {student: random.randint(1,100) for student in names}
-passed_students = {student: score for (student, score) in students_score.items() if score > 60}
+# names = ["Alex", "Beth", "Caroline", "Dave", "Eleanor", "Freddie"]
+# students_score = {student: random.randint(1,100) for student in names}
+# passed_students = {student: score for (student, score) in students_score.items() if score > 60}
+
+student_dict = {
+    "student": ["Angela", "James", "Lily"],
+    "score": [56, 76, 98]
+}
+
+import pandas
+
+student_data_frame = pandas.DataFrame(student_dict)
+print(student_data_frame)
+
+for (index, row) in student_data_frame.iterrows():
+    print(f"{row.student}: {row.score}")
