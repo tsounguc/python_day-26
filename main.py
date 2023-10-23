a95382ecf374661890fd3766c1f19f00bf77e5cd
Christian Tsoungui NKoulou <tsounguc@mail.gvsu.edu>
2023-10-22 21:23:12 -0400

    Conditional list comprehension formula

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 # new_numbers = [new_item for item in list]
 
 # Practice
-numbers = [1,2,3]
+# numbers = [1,2,3]
+#
+# new_numbers = [n + 1 for n in numbers]
+#
+# name = "Angela"
+#
+# new_list = [letter for letter in name]
+#
+# double_numbers = [number * 2 for number in range(1,5)]
+#
 
-new_numbers = [n + 1 for n in numbers]
-
-name = "Angela"
-
-new_list = [letter for letter in name]
-
-double_numbers = [number * 2 for number in range(1,5)]
+# Conditional List Comprehension
+# new_list = [new_item for item in list if test]
